ac0db389ad0c8a8e2f2b684c73e62c4a25025a4a
divya-codes-code <mashumkumari97@gmail.com>
2025-02-22 10:10:06 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 144553
+let accountEmail = "divya13756@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+
+console.table([accountID , accountPassword, accountEmail,accountCity, accountState]);
+
+/* prefer not to use var because of issue in block  scope & functional scope */
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version//
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log("Divya")
+
+console.log(4 + 5)
+
+let name = "Divya"
+let age = 20
+let isLoggedIn = true
+
+/*PRIMITIVE DATATYPES */
+//numeric => 2 to power 53
+//bigint
+//string =>""
+// boolean => true/false
+//null => standalone value
+//undefined 
+//symbol =>unique
+
+//object
+console.log(typeof "divya")
+
+console.log(typeof age)
+
+console.log(typeof null)//object
+
+console.log(typeof undefined) //undefined
