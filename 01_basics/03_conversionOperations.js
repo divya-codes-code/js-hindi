1ad174d2cb4f36185e6dfed6a2adf4276a60d73a
divya-codes-code <mashumkumari97@gmail.com>
2025-02-23 08:10:17 +0000

    container config done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log(score);
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+//"true" => 1 false => 0
+
+let isLoggedIn = "Divya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+//"" => false
+//"divya" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
