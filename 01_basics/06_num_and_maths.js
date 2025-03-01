49ee6608f3fd0584d4a39b9122e1b77a998afca9
divya-codes-code <mashumkumari97@gmail.com>
2025-03-01 08:16:41 +0000

    container config done

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,37 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance);
+
+
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+
+//console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+//console.log(otherNumber.toPrecision(3))
+
+const hundred = 10000000
+//console.log(hundred.toLocaleString);
+//console.log(hundred.toLocaleString('en-IN'));
+
+//++++++++++++++++++++   MATHS ++++++++++++++++++++////////
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.min(3,4,6,9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max =20
+
+Math.random() 
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
