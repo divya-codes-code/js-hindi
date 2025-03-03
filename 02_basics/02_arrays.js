76ec2e48e74bdccafddcbdc83802b192e7870221
divya-codes-code <mashumkumari97@gmail.com>
2025-03-03 17:39:50 +0000

    container config done

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -4,3 +4,21 @@ const dc_heros= ["spiderman", "batsman", "flash"]
 
 marvel_heros.push(dc_heros);
 console.log(marvel_heros);
+console.log(marvel_heros[3][1]);
+
+marvel_heros.concat(dc_heros)
+console.log(marvel_heros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+console.log(another_array);
+
+const real_another_array = another_array.flat(Infinity)
+console.log(another_array);
+console.log(real_another_array);
+
+console.log(Array.isArray("DIVYA"))
+console.log(Array.from("Divya"))
+console.log(Array.from({name:"Divya"}))
