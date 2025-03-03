a41a267a2e6c7c6c82ee94fd306d7bbd76a8e71d
divya-codes-code <mashumkumari97@gmail.com>
2025-03-03 20:44:18 +0000

    container config done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,27 @@
+//singleton
+//object.create
+
+//object literals
+
+const mySym = Symbol("key1")
+
+const jsuser={
+    name: "Divya",
+    "full name" : "Divya kumari",
+    [mySym]: "mykey1",
+    age: 20,
+    location: "jaipur",
+    email: "kumaridivya13756@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+
+}
+console.log(jsuser["email"])
+console.log(jsuser["full name"])
+console.log(jsuser[mySym])
+
+jsuser.email = "divya@chatgpt.com"
+Object.freeze(jsuser)
+jsuser.email = "divya@.com"
+
+console.log(jsuser)
