5b2f25a4aac9dd2506ed6e5719a17dcd78f354c9
divya-codes-code <mashumkumari97@gmail.com>
2025-03-04 18:08:12 +0000

    container config done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+ tinderUser.id = "123abc"
+tinderUser.name = "Divya"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser)
+
+//obkect inside object
+
+const regularUser = {
+    email: "divyakumari@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Divya",
+            lastname: "singh"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj4 = {5: "e", 6: "f"}
+
+//const obj3 = {obj1 , obj2}
+//const obj3= Object.assign(obj1, obj2,obj4)
+
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email: "divya@gmail.com"
+    },
+    {
+        id:1,
+        email: "divya@gmail.com"
+    },
+    {
+        id:1,
+        email: "divya@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
