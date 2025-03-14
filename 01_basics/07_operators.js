a43a029afa12e5db1d91150107b92743cef132fd
Divya Kumari <mashumkumari97@gmail.com>
2025-03-14 20:12:00 +0000

    container config done

---
+++ 01_basics/07_operators.js
@@ -0,0 +1,48 @@
+//Arthmetic operators
+
+let a=90;
+let b=5;
+console.log(a+b);
+console.log("a-b", a-b);
+console.log("a*b = ", a*b);
+console.log("a/b =", a/b);
+console.log("a%b", a%b);
+console.log("a**b", a**b);
+
+//unary operators
+
+let c =90;
+let d =5;
+console.log("c =" ,c, "& d = ", d);
+c++;
+console.log(c);
+d--;
+console.log(d);
+
+//Assignment operators
+let p =90;
+let q =5;
+c += 4
+console.log("p = ", p);
+d %= 4
+console.log("q = ", q);
+
+//comparision operators
+let s =90;
+let t ="5";
+
+console.log("s==t", s==t);
+console.log("s!=t", s!=t);
+console.log("s==t", s==t);
+console.log("s===t", s===t);
+
+//logical operators
+
+let x =50;
+let y =23;
+
+let cond1 = x > y;
+let cond2 = x < y;
+console.log("cond1 && cond2", cond1 && cond2);
+console.log("cond1 || cond2", cond1 || cond2);
+console.log("!(50>23) = ",!(x===50) );
