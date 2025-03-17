e01dbb3835c57650d784f91e7183c4f3299026b6
Divya Kumari <mashumkumari97@gmail.com>
2025-03-17 19:33:10 +0000

    container config done

--- 04_basics/one.js
+++ 04_basics/one.js
@@ -1,24 +1,21 @@
-//conditional statement
-
+// Conditional statement
 let age = 18;
 if (age >= 18) {
     console.log("You can vote"); 
 } 
 
-let age2 = 18;
-if (age2 <= 18) {
+let age2 = 17;
+if (age2 < 18) {
     console.log("You cannot vote"); 
-} 
+}
 
 let age3 = 18;
 let mode = "dark";
-let color;
+let color = "white"; // Default color
 
-if (mode === "dark"){
+if (mode === "dark") {
     color = "black"; 
-}
-
-if (mode === "light"){
+} else if (mode === "light") {
     color = "red"; 
 }
 console.log(color);
@@ -27,14 +24,12 @@ let age4 = 18;
 if (age4 >= 18) {
     console.log("You can vote"); 
 } else {
-    console.log("you cannot vote");   
+    console.log("You cannot vote");   
 }
 
-//odd or even 
-//if-else statement
-
+// Odd or Even
 let num = 10;
-if(num%2 === 0){
+if (num % 2 === 0) {
     console.log("even");   
 } else {
     console.log("odd");   
@@ -47,9 +42,7 @@ if (num2 % 2 === 0) {
     console.log("odd");
 }
 
-//syntax => rules
-// else-if statement
-
+// Else-if statement
 let marks = 85;
 
 if (marks >= 90) {
@@ -64,11 +57,35 @@ if (marks >= 90) {
 
 let Age = 45;
 
-if(Age<18){
-    console.log("junior");
-} else if (age >60) {
-    console.log("senior");    
+if (Age < 18) {
+    console.log("Junior");
+} else if (Age > 60) { // Fixed variable name
+    console.log("Senior");    
 } else {
-    console.log("middle");
-    
+    console.log("Middle");
+}
+
+// Ternary Operator
+let age5 = 25;
+let result = age5 >= 18 ? "adult" : "not adult"; 
+console.log(result); 
+
+let age6 = 60;
+age6 >= 18 ? console.log("adult") : console.log ("not adult"); 
+
+let age7 = 16;
+console.log(age7 >= 18 ? "adult" : "not adult"); // Fixed unused expression
+
+// Grade System
+let marks2 = 5;
+if (marks2 >= 80) {
+    console.log("Grade: A");
+} else if (marks2 >= 70) {
+    console.log("Grade: B");  
+} else if (marks2 >= 60) {
+    console.log("Grade: C");
+} else if (marks2 >= 50) {
+    console.log("Grade: D");
+} else {
+    console.log("Grade: F");
 }
