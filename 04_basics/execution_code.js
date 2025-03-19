b8991418b348f565db7f16d2211cbd35bfd0a843
Divya Kumari <mashumkumari97@gmail.com>
2025-03-19 18:55:14 +0000

    container config done

---
+++ 04_basics/execution_code.js
@@ -0,0 +1,15 @@
+let val1 = 10;
+let val2 = 5;
+function addNum(num1, num2){
+    let total = num1 + num2
+    return total
+}
+
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 2)
+
+//Execution Context
+// 1)Memory Execution Context - memory allocate for declare not executed
+//   2) Global Execution Context  3) Eval Execution Context
+
+// new environment + execution thread 
